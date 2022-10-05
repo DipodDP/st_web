0f0b2cfcddcaaf30d40d7fd07a239744dbd3c0d4
DipodDP <dipoddp@gmail.com>
2022-10-05 12:38:08 +0700

    Update s.py

--- s.py
+++ s.py
@@ -1,14 +1,15 @@
 import socket
-
+ 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
-s.listen(10)
+s.listen(1)
+client, addr = s.accept()
 while True:
-  conn, addr = s.accept()
-  while True:
-    data = conn.recv(1024)
-    if data == 'close':
-      conn.close()
-    
-    if not data: break
-    conn.send(data)
+    data = client.recv(1024)
+    if len(data) > 1024 or data == 'close':
+        client.close()
+        break
+    else:
+        client.send(data)
+        print data
+    s.close()
